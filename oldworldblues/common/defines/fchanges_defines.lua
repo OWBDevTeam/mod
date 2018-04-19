8e51b8c4b0443367f70e90ac516cccfe9b7be13f
garnithor <37359254+garnithor@users.noreply.github.com>
2018-04-19 16:17:39 +0200

    Branch Update tech

---
+++ oldworldblues/common/defines/fchanges_defines.lua
@@ -0,0 +1,6 @@
+NDefines.NProduction.BASE_FACTORY_MAX_EFFICIENCY_FACTOR = 35
+NDefines.NProduction.BASE_FACTORY_START_EFFICIENCY_FACTOR = 7
+NDefines.NCountry.MAJOR_IC_RATIO = 5                             -- difference in total factories needed to be considered major with respect to other nation
+NDefines.NCountry.MAJOR_MIN_FACTORIES = 40						-- need at least these many factories to become a major
+NDefines.NCountry.MIN_MAJOR_COUNTRIES	= 3						-- MIN_MAJOR_COUNTRIES countries with most factories will be considered as major countries
+NDefines.NCountry.ADDITIONAL_MAJOR_COUNTRIES_IC_RATIO = 0.75
